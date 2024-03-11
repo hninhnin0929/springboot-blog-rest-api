30b0ef861e7a6769303d522ddd93a4c4219b89f6
hnin <hnin0929@ecplaza.net>
2024-03-11 15:51:14 +0900

    post service impl

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -0,0 +1,40 @@
+package com.springboot.blog.springbootblogrestapi.service.impl;
+
+import com.springboot.blog.springbootblogrestapi.entity.Post;
+import com.springboot.blog.springbootblogrestapi.payload.PostDto;
+import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
+import com.springboot.blog.springbootblogrestapi.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        // convert DTO to entity
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post newPost = postRepository.save(post);
+
+        // convert entity to DTO
+        PostDto postResponse = new PostDto();
+        postResponse.setId(newPost.getId());
+        postResponse.setTitle(newPost.getTitle());
+        postResponse.setDescription(newPost.getDescription());
+        postResponse.setContent(newPost.getContent());
+
+        return postResponse;
+    }
+}
