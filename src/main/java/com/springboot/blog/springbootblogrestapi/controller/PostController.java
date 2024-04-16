8548f1aa7871fe7928eb469940b15a80facdf398
hnin <hnin0929@ecplaza.net>
2024-04-16 17:59:03 +0900

    posts get api

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -1,13 +1,13 @@
 package com.springboot.blog.springbootblogrestapi.controller;
 
+import com.springboot.blog.springbootblogrestapi.entity.Post;
 import com.springboot.blog.springbootblogrestapi.payload.PostDto;
 import com.springboot.blog.springbootblogrestapi.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -23,5 +23,11 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
         return  new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+//    get all posts rest api
+    @GetMapping
+    public List<PostDto> getAllPosts() {
+        return postService.getAllPosts();
     }
 }
--- src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -7,6 +7,9 @@ import com.springboot.blog.springbootblogrestapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -21,20 +24,38 @@ public class PostServiceImpl implements PostService {
     public PostDto createPost(PostDto postDto) {
 
         // convert DTO to entity
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+
+        // convert entity to DTO
+        PostDto postResponse = mapToDTO(newPost);
+
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+
+//    convert Entity into DTO
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+
+//    convert DTO into Entity
+    private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
-
-        Post newPost = postRepository.save(post);
-
-        // convert entity to DTO
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+        return post;
     }
 }
