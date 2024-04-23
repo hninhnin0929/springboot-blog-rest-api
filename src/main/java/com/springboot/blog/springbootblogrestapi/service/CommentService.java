aa91e1ac397b087a3b900361ae3e1a7b9a24a6c6
hnin <hnin0929@ecplaza.net>
2024-04-23 15:44:56 +0900

    create comment and get comments by post id

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.springboot.blog.springbootblogrestapi.service;
+
+import com.springboot.blog.springbootblogrestapi.payload.CommentDto;
+
+import java.util.List;
+
+public interface CommentService {
+
+    CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(long postId);
+}
---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
@@ -0,0 +1,70 @@
+package com.springboot.blog.springbootblogrestapi.service.impl;
+
+import com.springboot.blog.springbootblogrestapi.entity.Comment;
+import com.springboot.blog.springbootblogrestapi.entity.Post;
+import com.springboot.blog.springbootblogrestapi.exception.ResourceNotFoundException;
+import com.springboot.blog.springbootblogrestapi.payload.CommentDto;
+import com.springboot.blog.springbootblogrestapi.repository.CommentRepository;
+import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
+import com.springboot.blog.springbootblogrestapi.service.CommentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public CommentDto createComment(long postId, CommentDto commentDto) {
+
+        Comment comment = mapToEntity(commentDto);
+
+        /*retrieve post entity by id*/
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "id", postId));
+
+        /*set post to comment entity*/
+        comment.setPost(post);
+
+        /*comment entity to DB*/
+        Comment newComment = commentRepository.save(comment);
+
+        return mapToDTO(newComment);
+    }
+
+    @Override
+    public List<CommentDto> getCommentsByPostId(long postId) {
+        /*retrieve comments by postId*/
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        /*convert list of comment entities to list of comment dto's*/
+        return comments.stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
+
+    private CommentDto mapToDTO(Comment comment){
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        commentDto.setBody(comment.getBody());
+        return commentDto;
+    }
+
+    private Comment mapToEntity(CommentDto commentDto){
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        comment.setBody(commentDto.getBody());
+        return comment;
+    }
+}
