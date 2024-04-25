0e509bda54718e2ca5a1a72a3f08fb2369568134
hnin <hnin0929@ecplaza.net>
2024-04-25 11:04:18 +0900

    update comment by id

--- src/main/java/com/springboot/blog/springbootblogrestapi/service/CommentService.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/CommentService.java
@@ -11,4 +11,6 @@ public interface CommentService {
     List<CommentDto> getCommentsByPostId(long postId);
 
     CommentDto getCommentById(Long postId, Long commentId);
+
+    CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest);
 }
