2226d0f346407896106071dedf658c21ba89db2a
hnin <hnin0929@ecplaza.net>
2024-05-14 16:05:26 +0900

    securing rest api's with database authentication

--- src/main/java/com/springboot/blog/springbootblogrestapi/security/CustomUserDetailsService.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/security/CustomUserDetailsService.java
@@ -24,7 +24,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
 
-        User user = userRepository.findByUsernameorEmail(usernameOrEmail, usernameOrEmail)
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
                 .orElseThrow(() ->
                         new UsernameNotFoundException("User not found with username or email: "+ usernameOrEmail));
 
