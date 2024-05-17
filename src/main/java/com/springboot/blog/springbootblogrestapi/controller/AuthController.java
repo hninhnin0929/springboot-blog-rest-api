d040c273e72ec6263d50eda55c50384478d9f108
hnin <hnin0929@ecplaza.net>
2024-05-17 14:45:39 +0900

    build register REST API

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/AuthController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.springbootblogrestapi.controller;
 
 import com.springboot.blog.springbootblogrestapi.payload.LoginDto;
+import com.springboot.blog.springbootblogrestapi.payload.RegisterDto;
 import com.springboot.blog.springbootblogrestapi.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,5 +25,12 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
         String response = authService.login(loginDto);
         return ResponseEntity.ok(response);
+    }
+
+    /*Build Register REST API*/
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/AuthServiceImpl.java
@@ -1,20 +1,41 @@
 package com.springboot.blog.springbootblogrestapi.service.impl;
 
+import com.springboot.blog.springbootblogrestapi.entity.Role;
+import com.springboot.blog.springbootblogrestapi.entity.User;
+import com.springboot.blog.springbootblogrestapi.exception.BlogAPIException;
 import com.springboot.blog.springbootblogrestapi.payload.LoginDto;
+import com.springboot.blog.springbootblogrestapi.payload.RegisterDto;
+import com.springboot.blog.springbootblogrestapi.repository.RoleRepository;
+import com.springboot.blog.springbootblogrestapi.repository.UserRepository;
 import com.springboot.blog.springbootblogrestapi.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager,
+                           UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -27,5 +48,34 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return "User Logged-in successfully!";
+    }
+
+    @Override
+    public String register(RegisterDto registerDto) {
+
+        /*add check for username exists in database*/
+        if(userRepository.existsByUsername(registerDto.getUsername())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Username is already existed!");
+        }
+
+        /*add check for email exists in database*/
+        if(userRepository.existsByEmail(registerDto.getEmail())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Email is already existed!");
+        }
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User registered successfully!";
     }
 }
