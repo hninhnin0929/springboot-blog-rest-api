da9e8bedcffc0be7ffa9d0b64d18ed95c7a5da3b
hnin <hnin0929@ecplaza.net>
2024-07-01 14:13:50 +0900

    Build Add Category REST API and Secure using ADMIN role

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.springboot.blog.springbootblogrestapi.service.impl;
+
+import com.springboot.blog.springbootblogrestapi.entity.Category;
+import com.springboot.blog.springbootblogrestapi.payload.CategoryDto;
+import com.springboot.blog.springbootblogrestapi.repository.CategoryRepository;
+import com.springboot.blog.springbootblogrestapi.service.CategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryRepository categoryRepository;
+    private ModelMapper modelMapper;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper) {
+        this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public CategoryDto addCategory(CategoryDto categoryDto) {
+
+        Category category = modelMapper.map(categoryDto, Category.class);
+        Category savedCategory = categoryRepository.save(category);
+
+        return modelMapper.map(savedCategory, CategoryDto.class);
+    }
+}
