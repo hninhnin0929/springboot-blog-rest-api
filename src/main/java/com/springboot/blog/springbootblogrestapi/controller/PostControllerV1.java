fe41aac630fce7a5856d6fe4217d64dc2ed89410
DESKTOP-M58DAPE\user <hninhnin0929>
2024-09-21 18:22:35 +0900

    Versioning through custom headers

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostControllerV1.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostControllerV1.java
@@ -70,13 +70,13 @@ public class PostControllerV1 {
     }
 
     //    get post by id
-    @GetMapping(value = "/api/posts/{id}", params = "version=1")
+    @GetMapping(value = "/api/posts/{id}", headers = "X-API-VERSION=1")
     public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
     //    get post by id
-    @GetMapping(value = "/api/posts/{id}", params = "version=2")
+    @GetMapping(value = "/api/posts/{id}", headers = "X-API-VERSION=2")
     public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = "id") long id) {
         PostDto postDto = postService.getPostById(id);
         PostDtoV2 postDtoV2 = new PostDtoV2();
