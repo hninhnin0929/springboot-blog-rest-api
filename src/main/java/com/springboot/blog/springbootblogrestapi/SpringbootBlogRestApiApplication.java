1e8f76249dba49c45e3922cdda86d8ee6b3b8cb3
DESKTOP-M58DAPE\user <hninhnin0929>
2024-09-28 23:11:19 +0900

    Using Profiles in Spring Boot Blog App

--- src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import com.springboot.blog.springbootblogrestapi.entity.Role;
+import com.springboot.blog.springbootblogrestapi.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +35,7 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/hninhnin0929/springboot-blog-rest-api"
 		)
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -42,4 +46,18 @@ public class SpringbootBlogRestApiApplication {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
